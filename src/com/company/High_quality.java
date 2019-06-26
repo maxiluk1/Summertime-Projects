3590940d5f6341d939270cacde9d825a69ba3c45
Damian <damekk1@gmail.com>
2019-06-26 22:56:40 +0200

    Change a repository

--- src/com/company/High_quality.java
+++ src/com/company/High_quality.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class High_quality {
     public static void main(String[] args) {
-        System.out.println("Qualiety content");
+        System.out.println("Qualdiety content");
     }
 }
