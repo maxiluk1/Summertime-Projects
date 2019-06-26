9654cf5fd48818e2920157790faf9d7355830d48
Damian <damekk1@gmail.com>
2019-06-26 22:52:01 +0200

    Change a repository

--- src/com/company/Second.java
+++ src/com/company/Second.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Second {
     public static void main(String[] args) {
-        System.out.println("Nadchodza zmiany");
+        System.out.println("Nadchodza zmieany");
     }
 }
--- src/com/company/TxtToJava.java
+++ src/com/company/TxtToJava.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class TxtToJava {
     public static void main(String[] args) {
-        System.out.println("Try pull it");
+        System.out.println("Try peull it");
     }
 }
